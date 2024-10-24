de2bf6e82451e3073c8a004e97fc95fba93188e4
tkj3nfuq <thinphcv5@gmail.com>
2024-10-24 22:18:52 +0700

    refactor: Update default port to 80 in index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 80;
 const databaseURL = process.env.DB_URL;
 
 app.use(cors());
