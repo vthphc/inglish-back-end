bb229b9834ac8453bd03d2b9592fa65146f7cacd
tkj3nfuq <thinphcv5@gmail.com>
2024-10-28 10:15:34 +0700

    Added userId for phrases' schema

--- models/phrases.js
+++ models/phrases.js
@@ -2,6 +2,10 @@ const mongoose = require("mongoose");
 
 const phraseSchema = new mongoose.Schema(
     {
+        userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        },
         topic: String,
         dialogues: [
             {
