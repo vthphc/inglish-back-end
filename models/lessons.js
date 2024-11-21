027484d9b5b7bbce74528e027eb8e54f78316401
Mingu <vhnm3004@outlook.com>
2024-11-21 20:24:31 +0700

    Refactor

--- models/lessons.js
+++ models/lessons.js
@@ -1,22 +1,21 @@
 const mongoose = require("mongoose");
 
 const lessonSchema = new mongoose.Schema(
-    {
-        title: String,
-        type: String,
-        contentURL: String,
-        questions: [
-            {
-                questionName: String,
-                questionOptions: [String],
-                correctAnswer: String,
-                _id: false,
-            },
-        ],
-        AIExplaination: String,
-        createdAt: Date,
-    },
-    { versionKey: false }
+	{
+		title: String,
+		type: String,
+		contentURL: String,
+		questions: [
+			{
+				questionName: String,
+				questionOptions: [String],
+				correctAnswer: String,
+			},
+		],
+		AIExplaination: String,
+		createdAt: Date,
+	},
+	{ versionKey: false }
 );
 
 module.exports = mongoose.model("Lesson", lessonSchema);
